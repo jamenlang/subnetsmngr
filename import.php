bc7ecb6833fc0f522f15fe38fed06da4f2f37f02
Jamen <jamen.lang@gmail.com>
2017-01-16 13:26:48 -0700

    Update import.php
    update update query to support v3 created data.

--- import.php
+++ import.php
@@ -1,6 +1,28 @@
 <?php
+/*
+ * subnetsmngr
+ * Author(s): Sean Kennedy <sean@kndy.net> 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ */
+
 require('inc/global.php');
+
 if (!isset($_GET['subnet_id'])) exit;
+
 if ($_POST){
 
 	$lines = explode("\r\n",$_POST['input']);
@@ -9,13 +31,13 @@ if ($_POST){
 		$column_order = explode(',',$_POST['column_order']);
 		//the data is now in 3 columns.
 		if($_POST['delimiter'] == '  '){
-      /* hack, excel columns were not copied with two or more spaces before the ip column. */
+      			/* hack, excel columns were not copied with two or more spaces before the ip column. */
 			$line = preg_replace('/\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s/','#$#' . "$1" . '#$#',$line,1);
 			$line = preg_replace('/\s{2,}/','#$#',$line);
 			$line = preg_replace('/#\$##\$#/','#$#',$line);
 			$new_delimiter = '#$#';
 		}
-    //make these sticky for next import
+    		//make these sticky for next import
 		$_SESSION['delimiter'] = $_POST['delimiter'];
 		$_SESSION['column_order'] = $_POST['column_order'];
 
@@ -31,28 +53,45 @@ if ($_POST){
 		}
 		if(stristr($ip,'-'))
 			continue;
-		if(!$ip)
+		if(!$ip){
+			$_SESSION['error_msg'] .= 'Host missing in ' . $description . '/' . $notes . ' line, skipping.';
 			continue;
+		}
 		
 		$description = pg_escape_string($description);
 		$notes = pg_escape_string($notes);
 		
 		if (!ip_in_network($ip, $_POST['subnet_addr']))
-			$_SESSION['error_msg'] = 'Host ' . $ip . ' is not within subnet ' . $_POST['subnet_addr'] . '.';
+			$_SESSION['error_msg'] .= 'Host ' . $ip . ' is not within subnet ' . $_POST['subnet_addr'] . '.';
 		else {
 			// host exist in db yet?
 			$sql = "select id from hosts where addr = '{$ip}'";
 			$result = @pg_query($sql) or die("error: failed to run query: $sql");
 			if (pg_num_rows($result) == 1) {
-				if (isset($_POST['new'])){
-					$_SESSION['notice_msg'] .= 'Host ' . $ip . ' already exists, updating if necessary.';
-					$sql = "update hosts set free = '" . (isset($_POST['used']) ? '0' : '1') . "', description = '{$description}', notes = '{$notes}', last_updated = now(), last_updated_user_id = '{$_SESSION['user']['id']}', gateway = '" . (isset($_POST['gateway']) && $_POST['gateway'] ? 1 : 0) . "' where addr = '{$ip}'";
+				if (isset($_POST['overwrite'])){
+					$_SESSION['notice_msg'] .= 'Host ' . $ip . ' already exists, overwriting.';
+					$sql = "update hosts set free = '" . (isset($_POST['used']) ? '0' : '1') . "', description = '$desc', " .
+						"notes = '$_POST[notes]', last_updated = now(), last_updated_user_id = '{$_SESSION['user']['id']}', gateway = '" . 
+						(isset($_POST['gateway']) && $_POST['gateway'] ? 1 : 0) . "' where addr = '{$ip}' and subnet_id = '$_GET[subnet_id]'";
 				}
 			} else
-				$sql = "insert into hosts (subnet_id, addr, free, gateway, description, notes, last_updated, last_updated_user_id) values('{$_GET['subnet_id']}', '{$ip}', '" . (isset($_POST['used']) ? '0' : '1')  . "', '" . (isset($_POST['gateway']) ? 1 : 0) . "', '{$description}', '{$notes}', now(), '{$_SESSION['user']['id']}')";
+			{
+				$sql = "insert into hosts (subnet_id, addr, free, gateway, description, notes, last_updated, last_updated_user_id, created, created_user_id) values(
+					'{$_GET['subnet_id']}', 
+					'{$_POST['addr']}', 
+					'" . (isset($_POST['used']) ? '0' : '1')  . "', 
+					'" . (isset($_POST['gateway']) ? 1 : 0) . "', 
+					'" . $desc . "',
+					'{$_POST['notes']}',
+					now(), 
+					'{$_SESSION['user']['id']}',
+					now(), 
+					'{$_SESSION['user']['id']}')";	
+			}
 			if (!isset($_SESSION['error_msg']) || !$_SESSION['error_msg']) {
 				@pg_query($sql) or die("error: failed to run query: $sql: " . pg_last_error());
-			/*
+
+				/*
 				if (isset($_POST['ptr']) && ($_POST['ptr'] != $_POST['old_ptr'])) {
 					if (!$_POST['ptr_record_id']) {
 						if (ip_version($_POST['addr']) == 4) {
@@ -136,12 +175,12 @@ if ($_POST){
 						}
 					}
 				}
-			*/
+				*/
 				$_SESSION['notice_msg'] .= 'Host <a href="hosts.php?subnet_id=' . $_GET['subnet_id'] . '#' . $ip . '">' . $ip . '</a> updated.';
 			}
 		}
 	}
-  if (isset($_GET['ref']) && $_GET['ref'])
+	if (isset($_GET['ref']) && $_GET['ref'])
 		header("Location: {$_GET['ref']}");
 	else
 		header("Location: hosts.php?subnet_id={$_GET['subnet_id']}");
@@ -151,7 +190,6 @@ if ($_POST){
 $subnet_tree = subnet_get_tree($_GET['subnet_id']);
 $subnet_info = $subnet_tree[0];
 $parent_subnet_info = isset($subnet_tree[1]) ? $subnet_tree[1] : null;
-//print_r($parent_subnet_info['notes']);
 
 $sql = "select addr from hosts where gateway = '1' and subnet_id = '{$_GET['subnet_id']}'";
 $result = @pg_query($sql) or die("error: failed to run query: $sql");
@@ -206,6 +244,8 @@ echo '<tr><td width="180" valign="top"><b>Delimiter:</b></td><td><input type="te
 echo '<tr><td width="180" valign="top"><b>Column Order:</b></td><td><input type="text" name="column_order" value="' . (($_SESSION['column_order']) ? $_SESSION['column_order'] : 'notes,ip,description' ) . '"></td></tr>';
 echo '<tr><td><b>Mark as Used?</td><td><input type="checkbox" name="used" value="1"' . 
 	(ifsetor($_POST['used'], !ifsetor($row['free'])) ? ' checked="checked"' : '') . ' /></td></tr>';
+echo '<tr><td><b>Overwrite existing?</td><td><input type="checkbox" name="overwrite" value="1"' . 
+	(ifsetor($_POST['overwrite']) ? ' checked="checked"' : '') . ' /></td></tr>';
 echo '</table>';
 echo '<br /><br />';
 echo '<input type="submit" value="  ' . ($row ? 'IMPORT' : 'IMPORT') . ' Hosts  " />';
